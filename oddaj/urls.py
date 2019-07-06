a2dff32d827a872795bfaf4f3e4e84dda5489d79
Piotr Szydywar <pszydywar@gmail.com>
2019-07-07 01:49:22 +0200

    landing page added

--- oddaj/urls.py
+++ oddaj/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from myapp.views import IndexView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', IndexView.as_view(), name="index"),
+
 ]
