44b6b6f4d8111688ffcbd6159825d4dcb2b89062
Piotr Szydywar <pszydywar@gmail.com>
2019-07-10 23:24:07 +0200

    users app created

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
@@ -10,11 +10,21 @@ class IndexView(View):
         return render(request, 'myapp/index.html')
 
 
-class LoginView(View):
-    def get(self, request):
-        return render(request, 'myapp/login.html')
+# class LoginView(View):
+#     def get(self, request):
+#         return render(request, 'myapp/login.html')
 
 
 class RegisterView(View):
     def get(self, request):
         return render(request, 'myapp/register.html')
+
+    def post(self, request):
+        pass
+
+
+class AdminProfileView(LoginRequiredMixin, View):
+    def get(self, request):
+        users = User.objects.filter(is_superuser=True)
+        return render(request, 'myapp/admin_site.html', {'users': users})
+
--- oddaj/urls.py
+++ oddaj/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from myapp.views import IndexView, LoginView, RegisterView
+from myapp.views import IndexView, RegisterView, AdminProfileView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', IndexView.as_view(), name="index"),
-    path('login/', LoginView.as_view(), name="login"),
+    path('login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name="login"),
     path('register/', RegisterView.as_view(), name="register"),
+    path('admin_list/', AdminProfileView.as_view(), name="admin-list"),
 
 
 ]
